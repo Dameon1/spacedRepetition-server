c53e88191ab589a8c1783a6ac58d96690d61893b
Dameon1 <dameonmedoza@gmail.com>
2018-07-11 15:45:31 -0700

    changed questsions

--- questions/route.js
+++ questions/route.js
@@ -6,14 +6,19 @@ const Question = require('./model');
 const Users = require('../users/model');
 const passport = require('passport');
 
+let cl = (x,y) => {console.log(x,y);};
+
 let helperFunction = (user,answeredQuestion) => {
   let currentQuestion = answeredQuestion;
-  for (let i = 0; i < answeredQuestion.memoryValue && currentQuestion; i++) {
-    const nextIndex = currentQuestion.next;
+ 
+  let nextIndex= currentQuestion.next;
+  for (let i = 1; i < answeredQuestion.memoryValue && currentQuestion; i++) {
     currentQuestion = user.questions[nextIndex];
+    nextIndex = currentQuestion.next;
   }
-  answeredQuestion.next = currentQuestion.next;
+  answeredQuestion.next = nextIndex;
   currentQuestion.next = user.head;
+  return user;
 };
 
 
@@ -25,28 +30,41 @@ router.get('/', (req, res, next) => {
     .findById({_id:userId})
     .then(results => {
       return res.json(results.questions[results.head].question);
-    });
+    })
+    .catch(next);
 });
 
 router.post('/', (req, res, next) => {
   const { response  } = req.body;
   const userId = req.user.id;
+  
   Users
     .findById({_id:userId})
     .then(user => {
+      
       const answeredQuestionIndex = user.head;
       const answeredQuestion = user.questions[answeredQuestionIndex];
       if (response.toLowerCase() === answeredQuestion.answer.toLowerCase()){
-        answeredQuestion.memoryValue *= 2;
+        user.questions[answeredQuestionIndex].memoryValue *= 2;
+        user.markModified('questions');
         res.status(204).end();
       } 
       else {       
         res.json(answeredQuestion.answer);
       }
-      helperFunction(user, answeredQuestion);
-      user.head = answeredQuestion.next;
-      user.save();
-    });
+      user.save()
+        .then((updatedUser) =>  {
+          console.log(updatedUser);
+          return helperFunction(updatedUser, answeredQuestion);
+        })
+        .then((updatedUser) => {
+          updatedUser.head = answeredQuestion.next; 
+          return updatedUser.save();
+        })
+        .catch(next);
+
+    })
+    .catch(next);
 });
 
 module.exports = { router };
